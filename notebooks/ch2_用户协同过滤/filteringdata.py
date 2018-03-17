d903dd11fafdf483ceb97c8fa9aee90fe2cbffc2
heihei2015 <zhoutaor2013@163.com>
2018-03-17 20:23:14 +0800

    add some new code

---
+++ notebooks/ch2_用户协同过滤/filteringdata.py
@@ -0,0 +1,86 @@
+#coding:utf-8
+#  FILTERINGDATA.py
+#
+#  Code file for the book Programmer's Guide to Data Mining
+#  http://guidetodatamining.com
+#  Ron Zacharski
+#
+
+from math import sqrt
+
+users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+         "Jordyn":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+        }
+
+
+
+def manhattan(rating1, rating2):
+    """Computes the Manhattan distance. Both rating1 and rating2 are dictionaries
+       of the form {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
+    目的：计算两个样本的曼哈顿距离
+    rating1:用户样本1，字典类型
+    rating2：
+    返回值：两个样本的距离
+       """
+    distance = 0
+    commonRatings = False 
+    for key in rating1:
+        if key in rating2:
+            distance += abs(rating1[key] - rating2[key])
+            commonRatings = True
+    if commonRatings:
+        return distance
+    else:
+        return -1 #Indicates no ratings in common
+
+"""下面这种方法更简洁
+def manhattan(self, vector1, vector2):
+    return sum(map(lambda v1, v2: abs(v1 - v2), vector1, vector2))
+"""
+
+def computeNearestNeighbor(username, users):
+    """creates a sorted list of users based on their distance to username
+    目的：利用曼哈顿距离计算某个用户与其他所有用户的距离
+    username：某个用户名
+    users：所有的用户样本
+    返回值：某个用户与其他用户之间的距离，从小到大排序
+    """
+    distances = []
+    for user in users:
+        if user != username:
+            distance = manhattan(users[user], users[username])
+            distances.append((distance, user))
+    # sort based on distance -- closest first
+    distances.sort()
+    return distances
+
+def recommend(username, users):
+    """Give list of recommendations
+    目的：推荐最相邻的用户
+    username：某个用户名
+    users：所有的用户样本
+    返回值：username用户和与其最紧邻的用户
+    """
+    # first find nearest neighbor
+    nearest = computeNearestNeighbor(username, users)[0][1]
+
+    recommendations = []
+    # now find bands neighbor rated that user didn't
+    neighborRatings = users[nearest]
+    userRatings = users[username]
+    for artist in neighborRatings:
+        if not artist in userRatings:
+            recommendations.append((artist, neighborRatings[artist]))
+    # using the fn sorted for variety - sort is more efficient
+    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
+
+# examples - uncomment to run
+
+print( recommend('Hailey', users))
+#print( recommend('Chan', users))
